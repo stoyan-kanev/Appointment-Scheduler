8cb4b84e1c4b9c4ca87029cec9e1817d30133841
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-31 23:20:04 +0200

    Created model for users in the backend

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        max_length=255,
+    )
+    first_name = models.CharField(
+        unique=False,
+        blank=False,
+        max_length=20,
+    )
+    last_name = models.CharField(
+        unique=False,
+        blank=False,
+        max_length=20,
+    )
+    age = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+        default=0,
+        validators=[MinValueValidator(1), MaxValueValidator(100)],
+    )

63a8d135cdeedfe7d7d35a84963c674ff8b9d798
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-02-03 23:55:14 +0200

    Added Login Functionality

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../services/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -12,15 +13,25 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 export class LoginComponent {
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService
+    ) {
         this.loginForm = this.formBuilder.group({
             username: ['', Validators.required],
             password: ['', Validators.required]
         })
     }
-    submitForm(){
-        if(this.loginForm.valid){
 
+    submitForm() {
+        console.log(this.loginForm.value);
+        if (this.loginForm.valid) {
+            this.authService.login(this.loginForm.value).subscribe(
+                data => {
+                    this.loginForm.reset();
+                    console.log(data);
+                }
+            )
         }
     }
 }
--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,12 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {loginInfo} from '../types/login.interface';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    API_URL = 'http://localhost:8000/login';
+    API_URL = 'http://localhost:8000/users/';
+
     constructor(private http: HttpClient) {
 
     }
+
+    login(loginData: loginInfo): Observable<any> {
+        return this.http.post(this.API_URL + 'login', loginData, {withCredentials: true});
+    }
+
 }

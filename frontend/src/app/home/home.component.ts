7268d0aa73f4fbb16952074b2392d6f9be7165ae
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-02-07 23:39:46 +0200

    Added calendar to the home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        FormsModule
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    selectedDate: Date | null = null;
+    selectedTime: string = '';
+    showTimePicker = false;
 
+    timeSlots: string[] = [
+        '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM',
+        '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM',
+        '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM'
+    ];
+
+    onDateChange(event: any) {
+        this.selectedDate = event.value;
+        this.showTimePicker = true;
+    }
+
+    onTimeChange(event: any) {
+        this.selectedTime = event.value;
+    }
 }

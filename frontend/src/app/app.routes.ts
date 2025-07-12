4a8c1443b241fc81a322453ccaaca46025ebcd71
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-07-12 20:21:10 +0300

    Added new style to home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
-import {CalendarComponent} from './home/calendar.component';
+import {CalendarComponent} from './calendar/calendar.component';
 import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
-    {path: 'reserve', component: CalendarComponent},
+    {path: '', component: HomeComponent},
+    {path: 'book', component: CalendarComponent},
     {path: 'about', component: AboutComponent},
     {path: 'login', component: LoginComponent},
 ];

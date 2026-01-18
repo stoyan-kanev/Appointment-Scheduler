2db700c9fa3c351cf824ce03d4992b6487deeae2
Stoyan Kanev <74728512+stoyan-kanev@users.noreply.github.com>
2026-01-18 14:54:53 +0200

    Added footer and style for it

--- front-end-v2/src/app/app.ts
+++ front-end-v2/src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './nav/nav';
+import {FooterComponent} from './footer/footer';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [
-    RouterOutlet,
-    NavComponent,
-  ],
+    imports: [
+        RouterOutlet,
+        NavComponent,
+        FooterComponent,
+    ],
   styleUrl: './app.css'
 })
 export class App {

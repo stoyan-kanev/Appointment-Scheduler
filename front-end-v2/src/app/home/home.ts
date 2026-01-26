fcc63df73bc2eb45f743f7419104326653745b34
Stoyan Kanev <74728512+stoyan-kanev@users.noreply.github.com>
2026-01-26 19:52:11 +0200

    Added new style to v2

--- front-end-v2/src/app/home/home.ts
+++ front-end-v2/src/app/home/home.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+    imports: [
+        NgForOf,
+        NgOptimizedImage
+    ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class HomeComponent {
+    images = [
+        '/barber1.jpg',
+        '/barber2.jpg',
+        '/barber3.jpg',
+        '/barber4.jpg',
+        '/barber5.jpg',
+        '/barber6.jpg',
+        '/barber7.jpg',
+        '/barber8.jpg',
+        '/barber1.jpg',
+        '/barber2.jpg',
+        '/barber3.jpg',
+        '/barber4.jpg',
+        '/barber5.jpg',
+        '/barber6.jpg',
+        '/barber7.jpg',
+        '/barber8.jpg',
+    ];
+    currentSlide = 0;
+    intervalId: any;
+    constructor() {}
+    ngOnInit() {
+        this.intervalId = setInterval(() => {
+            this.currentSlide = (this.currentSlide + 1) % this.images.length;
+        }, 3000);
+    }
 
+    ngOnDestroy() {
+        clearInterval(this.intervalId);
+    }
 }
